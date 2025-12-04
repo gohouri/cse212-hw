f24a64972286312474f14d7ce527ce700e77235b
Gohouri <angejunior246@gmail.com>
2025-12-04 06:49:54 +0000

    W06 Code: Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,12 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+        {
+            // Value already exists, don't insert duplicates
+            return;
+        }
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +40,32 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        
+        if (value < Data)
+        {
+            // Search in the left subtree
+            if (Left is null)
+                return false;
+            return Left.Contains(value);
+        }
+        else
+        {
+            // Search in the right subtree
+            if (Right is null)
+                return false;
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
